6c350c8b1149fb0c175a3fb542bca9b5c66a8015
cas <casska@gmail.com>
2014-09-03 10:09:00 -0500

    fix - check

--- email_checker.py
+++ email_checker.py
@@ -8,10 +8,18 @@ import re
 with open(sys.argv[1], 'r') as f:
     for line in f:
         mail = re.search("(?<=<)(.*)(?=>)", line)
-        data = { 'email': mail }
-        data = urllib.urlencode(data)
-        response = urllib2.urlopen('http://mailtester.com/testmail.php', data)
-        print response.read()
+        if mail:
+            data = { 'email': mail.group(0) }
+            data = urllib.urlencode(data)
+            url = 'http://mailtester.com/testmail.php'
+            response = urllib2.urlopen(url, data)
+            r = response.read()
+            print r
+            valid = re.search( "E-mail address is valid", r )
+            if valid:
+
+                print "OK"
+##            print response.read()
 '''
 # print str(sys.argv[1])
 
